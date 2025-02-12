08a24e5a926036108b194d877a0646008930737b
JoaoLucasdCB <168235282+JoaoLucasdCB@users.noreply.github.com>
2025-02-11 21:02:56 -0300

    Delete JAVA/SENAI/ATIVIDADES/DoWhileEFor directory

--- JAVA/SENAI/ATIVIDADES/DoWhileEFor/DoWhileEFor34.java
+++
@@ -1,9 +0,0 @@
-public class DoWhileEFor34 {
-    public static void main(String[] args) {
-
-        int i = 10;
-        do {
-            System.out.print(i-- + " ");
-        } while (i >= 1);
-    }
-}
